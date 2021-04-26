4c78d1c847af5a61f289a6101915fede8efd7775
Swanand <swanand@educabiz.com>
2021-04-26 17:11:17 +0530

    Updated Root module and root component

--- web-client/src/main/resources/angular-client/src/app/landing/landing.module.ts
+++ web-client/src/main/resources/angular-client/src/app/landing/landing.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LandingRoutingModule } from './landing-routing.module';
+import { LandingHomeComponent } from './component/landing-home/landing-home.component';
+import { BrowserModule } from '@angular/platform-browser';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LandingHomeComponent
+  ],
   imports: [
+    BrowserModule,
     CommonModule,
     LandingRoutingModule
-  ]
+  ],
+  bootstrap: [LandingHomeComponent]
 })
 export class LandingModule { }
